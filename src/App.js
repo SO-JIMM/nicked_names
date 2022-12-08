9ca231c09ddc2a3512de1532f6c69732fae2e998
SO-JIMM <jamesdgrace@gmail.com>
2022-12-09 10:22:36 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+import names from "./names.json"
 
 function App() {
+  const [selected, setSelected] = React.useState(null)
+  const handleGetName = () => {
+    let rdm = Math.floor(Math.random() * names.length)
+    setSelected(names[rdm])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div>{selected && <p>{selected?.content}</p>}</div>
+        <button onClick={handleGetName}>Get Name</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
